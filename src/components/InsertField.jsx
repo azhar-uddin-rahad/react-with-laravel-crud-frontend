0980537d47449e1c6c6cf64782c55096e1b81fd8
Azhar Uddin Rahad <76016355+azhar-uddin-rahad@users.noreply.github.com>
2023-11-16 10:35:42 +0600

    post create successfully

--- src/components/InsertField.jsx
+++ src/components/InsertField.jsx
@@ -1,68 +1,72 @@
-import React, { useState } from 'react'
-import Api from './Api';
-import axios from 'axios';
+import React, { useState } from "react";
+import Api from "./Api";
+import axios from "axios";
 
 const InsertField = () => {
-  const {http}=Api();
-  const [info,setInfo]=useState({
-    title:"",
-    description: ""
-  })
+  const [info, setInfo] = useState({
+    title: "",
+    description: "",
+  });
+  const [error, setError] = useState({});
+  const [desError, setDesError] = useState("");
 
-  const [titleError,setTitleError]=useState("");
-  const [desError,setDesError]=useState("");
+  const handleValues=(e)=>{
+    setInfo({...info,
+      [ e.target.name] : e.target.value})
 
-/* const handleValues=(e)=>{
+}
+const handleSubmit=()=>{
+   console.log(info)
+
 
   const data={
     title:info.title,
     description:info.description
   }
   axios.post(`http://127.0.0.1:8000/api/createpost`,data).then(res=>{
-    alert(res.data.msg)
- 
+    alert(res.data.message)
   })
-  .catch(function(error){
+ .catch(function(error){
       if(error.response){
-          if(error.response.status ===422){
-            setTitleError(res.data.error.title);
-            setDesError(res.data.error.description);
-          }
-          if(error.response.status ===500){
-            alert(error.response.)
-          }
+          if(error.response.status === 422){
+            setError(error.response.data.errors);
+          }   
+          if(error.response.status === 500){
+            alert(error.response.error);
+            
+          }   
       }
-  })
-} */
- /*
-setInfo({...info,
-  [ e.target.name] : e.target.value}
-   )
- }
- const handleSubmit=()=>{
-   /* http.post('/createpost',{
-     title:info.title,
-     description:info.description
-   }).then(res=>{
-     console.log(res)
-     if(res.data.status === '422'){
-       setTitleError(res.data.error.title);
-       setDesError(res.data.error.description);
-     }
-   }) */
+  }) 
+  
+} 
   return (
-    <div className='max-w-[500px] mx-auto'>
-      <input onChange={handleValues} name="title" type="text" placeholder="Type here" className="input input-bordered w-full  mb-3" />
-      {/* {titleError ? <span className='text-red-500 text-xs mb-2'>"Title Field is Require"</span> :""} */}
+    <div className="max-w-[500px] mx-auto">
+      <input
+        onChange={handleValues}
+        name="title"
+        type="text"
+        placeholder="Type here"
+        className="input input-bordered w-full  mb-3"
+      />
+      <span className='text-red-500 text-xs mb-2'>{error.title}</span> 
       <br />
-      <textarea onChange={handleValues} className="textarea textarea-bordered w-full" name='description' placeholder="Bio"></textarea>
-      {/* {desError ? <span className='text-red-500 text-xs mb-2'>"Title Field is Require"</span> :""} */}
-       <div className='flex justify-center items-center'>
-       <button onClick={handleSubmit} className="btn btn-info text-center px-5 mt-2">Submit</button>
-       </div>
+      <textarea
+        onChange={handleValues}
+        className="textarea textarea-bordered w-full"
+        name="description"
+        placeholder="Bio"
+      ></textarea>
+     <span className='text-red-500 text-xs mb-2'>{error.description}</span> 
+      <div className="flex justify-center items-center">
+        <button
+          onClick={handleSubmit}
+          className="btn btn-info text-center px-5 mt-2"
+        >
+          Submit
+        </button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default InsertField
-
+export default InsertField;
